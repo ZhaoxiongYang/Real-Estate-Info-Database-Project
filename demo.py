5ba822fba8f534d2ddea737fe85ce5490436e6f4
ZhaoxiongYang <zhaoxiong.yang@gmail.com>
2018-03-05 17:34:01 -0800

    Add files via upload

--- demo.py
+++ demo.py
@@ -81,13 +81,6 @@ class Myparser_infor(HTMLParser):
             self.descrip = True
         if tag == 'div' and _attr(attrs, 'class', 'miniCol24 xsCol8 smlCol4 mdCol4 lrgCol4 pvmXxsVisible'):
             self.price_zip = True
-            # if len(attrs) == 0:
-            #     pass
-            # else:
-            #     for (variable, value) in attrs:
-            #         if variable == "data-role" and value == "address":
-            #             print("here")
-            #             self.address = True
 
     def handle_endtag(self, tag):
         if tag == 'div':
@@ -144,16 +137,6 @@ class Myparser_infor(HTMLParser):
             self.value['Average_price_sqft_for_zip'] = data.strip()
             self.price_zip_count += 1
 
-    # def handle_starttag(self, tag, attrs):
-    #     if tag == 'div' and _attr(attrs, 'data-title'):
-    #         movie = {}
-    #         movie['actors'] = _attr(attrs, 'data-actors')
-    #         movie['director'] = _attr(attrs, 'data-director')
-    #         movie['duration'] = _attr(attrs, 'data-dutation')
-    #         movie['title'] = _attr(attrs, 'data-title')
-    #         movie['rate'] = _attr(attrs, 'data-rate')
-    #         self.infors.append(movie)
-
 
 def Get_suburl(url):
     headers = {}
@@ -170,7 +153,6 @@ def Get_suburl(url):
         '121.69.8.234:8080',
     ]
     proxy = random.choice(proxy_list)
-    # proxy = proxy_list[8]
     urlhandle = urllib.request.ProxyHandler({'http': proxy})
     opener = urllib.request.build_opener(urlhandle)
     opener.addheaders = [
@@ -179,9 +161,7 @@ def Get_suburl(url):
     request = urllib.request.Request(url)
     response = urllib.request.urlopen(request)
     content = response.read().decode('utf-8')
-    # req = requests.get(url)
-    # s = req.text
-    # print(content)
+
     myparser = Myparser()
     myparser.feed(content)
     myparser.close()
@@ -193,6 +173,15 @@ def Get_infor(url):
 
     headers = {}
     proxy_list = [  # 这是我当时用的代理IP，请更新能用的IP
+        '208.95.62.81:3128',
+        '34.231.147.235:3128',
+        '54.37.18.122:3128',
+        '194.73.99.100:8080',
+        '12.221.240.25:8080',
+        '94.76.208.41:3838',
+        '81.128.197.194:3838',
+        '163.172.27.213:3128',
+        '159.203.166.201:8118',
         '12.221.240.25:8080',
         '35.196.26.166:3128',
         '216.177.233.181:8080',
@@ -203,13 +192,26 @@ def Get_infor(url):
         '211.71.20.246:3128',
         '115.182.83.38:8080',
         '121.69.8.234:8080',
+        '54.202.119.87:8080',
+        '144.202.49.154:8080',
+        '144.202.16.138:8080',
+        '159.89.195.153:8118',
+    ]
+    User_Agent = [
+        'Mozilla/5.0 (iPod; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3A101a Safari/419.3',
+        'Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
+        'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19',
+        'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0',
+        'Mozilla/5.0 (Android; Mobile; rv:14.0) Gecko/14.0 Firefox/14.0',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
     ]
     proxy = random.choice(proxy_list)
     # proxy = proxy_list[8]
     urlhandle = urllib.request.ProxyHandler({'http': proxy})
     opener = urllib.request.build_opener(urlhandle)
-    opener.addheaders = [
-        ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')]
+    Agent = random.choice(User_Agent)
+    opener.addheaders = [('User-Agent', Agent)]
     urllib.request.install_opener(opener)
     request = urllib.request.Request(url)
     response = urllib.request.urlopen(request)
@@ -230,70 +232,51 @@ if __name__ == '__main__':
     # url0 = 'https://www.trulia.com/CA/Los_Angeles'
     # urls, url_next = Get_suburl(url0)
     rows = []
-    headers = ['zipcode', 'address', 'cityState', 'neighborhood', 'price', 'type','beds', 'baths','built','space','lot_space','price_sqft', 'Average_Listing_Price_for_zip','Median_Sale_Price_for_zip','Average_price_sqft_for_zip','description']
-    with open('Infors.csv', 'w', newline='') as csvfile1:
-        spamwriter1 = csv.writer(
-            csvfile1, delimiter=" ", quoting=csv.QUOTE_MINIMAL)
-        spamwriter1.writerow("################################")
-        csvfile1.close()
+    headers = ['zipcode', 'address', 'cityState', 'neighborhood', 'price', 'type', 'beds', 'baths', 'built', 'space', 'lot_space',
+               'price_sqft', 'Average_Listing_Price_for_zip', 'Median_Sale_Price_for_zip', 'Average_price_sqft_for_zip', 'description']
+    # with open('Infors.csv', 'w', newline='') as csvfile1:
+    #     spamwriter1 = csv.writer(csvfile1, delimiter=" ", quoting=csv.QUOTE_MINIMAL)
+    #     # spamwriter1.writerow("################################")
+    #     spamwriter1.writerow(headers)
+    #     csvfile1.close()
+    # with open('Urls_error.csv', 'w') as csvfilex:
+    #             spamwriter = csv.writer(csvfilex)
+    #             spamwriter.writerow('////////////////////////////////')
+    #             csvfilex.close()
     spamReader = csv.reader(open('urls.csv', newline=''),
                             delimiter=' ', quotechar='|')
     count = 0
     for row in spamReader:
+        count += 1
+        if count <= 3415:
+            continue
         print("rows :", count)
         url = ''.join(row)
-        print(url)
         url = "https://www.trulia.com" + url
-        Infors = Get_infor(url)
-        count += 1
-        time.sleep(3)
-        r = ""
-        for x in Infors:
-            r += Infors[x] + ','
-        # print(r)
-        # rows.append(r)
-        with open('Infors.csv', 'a') as csvfile1:
-            spamwriter1 = csv.DictWriter(csvfile1, headers)
-            spamwriter1.writeheader()
-            spamwriter1.writerow(Infors)
-            csvfile1.close()
+        print(url)
+        try:
+            Infors = Get_infor(url)
+            # print(url)
+            # count += 1
+            if count % 10 == 0:
+                time.sleep(random.randint(5, 8))
+            if count % 50 == 0:
+                time.sleep(random.randint(10, 20))
+            time.sleep(random.randint(3, 6))
+            with open('Infors.csv', 'a') as csvfile1:
+                spamwriter1 = csv.DictWriter(csvfile1, headers)
+                spamwriter1.writerow(Infors)
+                csvfile1.close()
+        except UnicodeEncodeError:
+            with open('Urls_error.csv', 'a', newline='') as csvfile:
+                spamwriter = csv.writer(
+                    csvfile, delimiter=" ", quoting=csv.QUOTE_MINIMAL)
+                spamwriter.writerow(url)
+                csvfile.close()
+
+            print('row =', count, 'error')
+            continue
+        else:
+            print('row =', count, 'completed')
         # if count > 5:
         #     break
-
-    # count = 0
-    # for i in range(count + 5):
-    #     if i < len(urls):
-    #         url = "https://www.trulia.com" + urls[i]
-    #         Infors = Get_infor(url)
-    #         row = ""
-    #         for x in Infors:
-    #             if x == 'overiew':
-    #                 if 'Beds' in Infors[x] and 'Baths' in Infors[x]:
-    #                     row += Infors[x] + ','
-    #                 elif'Beds' in Infors[x]:
-    #                     p = Infors[x].find('Built')
-    #                     row += Infors[x][:p-1] +',' + Infors[x][p-1:]
-    #                 elif'Baths' in Infors[x]:
-    #                     p = Infors[x].find('Baths')
-    #                     row += Infors[x][:p-3] +',' + Infors[x][p-3:]
-    #                 else:
-    #                     p = Infors[x].find('Built')
-    #                     row += Infors[x][:p-1] +',,' + Infors[x][p-1:]
-    #             else:
-    #                 row += Infors[x] + ','
-    #         print(row)
-    #         rows.append(row)
-    #         with open('Infors.csv', 'a', newline='') as csvfile1:
-    #             spamwriter1 = csv.writer(csvfile1, delimiter=" ", quoting=csv.QUOTE_MINIMAL)
-    #             spamwriter1.writerows(rows)
-    #             csvfile1.close()
-    # count += 5
-    # for x in Infors:
-
-        # for url in urls:
-        #     # print(url)
-        #     url = url0 + url
-        # print(url)
-
-        # for each in infors:
-        #     print('%(title)s|%(rate)s|%(actors)s|%(director)s|%(duration)s' % each)
